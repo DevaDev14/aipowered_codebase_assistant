a62f0e86e37df7061f9b780041c2f28b5434e964
devaqspiders <devaqspiders@gmail.com>
2026-07-30 14:41:23 +0530

    c

--- services/extraction/git_extraction.py
+++ services/extraction/git_extraction.py
@@ -1,10 +1,14 @@
 from git import Repo
+from path_extraction import get_path
 
 def get_repo(repo_url, repo_name):
+    directory = fr"{repo_url}{repo_name}"
+    print(fr"{repo_url}/{repo_name}")
     Repo.clone_from(
         repo_url,
-        fr"D:\genai\codebase_assistant\git_repos\{repo_name}"
+        directory
     )
+    get_path(directory)
 
 if __name__ == '__main__':
     repo_url = r'https://github.com/encode/httpx'
